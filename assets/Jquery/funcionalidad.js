1efedf44a037d14ccd72fe09880933b7af15d203
Patgsj <patricio.gsj@gmail.com>
2024-08-19 10:55:35 -0400

    Funcionalidad resumen echa con  Javascript

---
+++ assets/Jquery/funcionalidad.js
@@ -0,0 +1,15 @@
+function mostrar(valor) {
+    document.getElementById("resultado").innerHTML = valor;
+}
+
+function emailRegistro(valor) {
+    document.getElementById("resultadoEmail").innerHTML = valor;
+}
+
+function MensajeRegistro(valor) {
+    document.getElementById("resultadoTextArea").innerHTML = valor;
+}
+
+
+
+
