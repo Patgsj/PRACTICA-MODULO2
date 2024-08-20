65f281561d9f052655c07792c4536b765149daf0
Patgsj <patricio.gsj@gmail.com>
2024-08-19 21:10:32 -0400

    cambio jquery

--- assets/Jquery/funcionalidadJquery.js
+++ assets/Jquery/funcionalidadJquery.js
@@ -16,13 +16,39 @@ $(document).ready(function() {
         event.preventDefault(); // Evita el envío del formulario
         
         // Obtener Valores
-        var nombre = $('#Nombre').val();
-        var email = $('#Email').val();
-        var mensaje = $('#floatingTextarea').val();
+        var nombre = $('#Nombre').val().trim(); // .trim() para eliminar espacios en blanco
+        var email = $('#Email').val().trim();
+        var mensaje = $('#floatingTextarea').val().trim();
         
-        // Captar los elemnetos del de los Inputs
-        $('#resultado').text(nombre);
-        $('#resultadoEmail').text(email);
-        $('#resultadoTextArea').text(mensaje);
+        // Verificar si los campos están completos
+        if (nombre === '' || email === '' || mensaje === '') {
+            
+            Swal.fire({
+                title: "Te faltan datos",
+                text: "Termina de completarlos",
+                icon: "warning",
+                showClass: {
+                  popup: `
+                    animate__animated
+                    animate__fadeInUp
+                    animate__faster
+                  `
+                },
+                hideClass: {
+                  popup: `
+                    animate__animated
+                    animate__fadeOutDown
+                    animate__faster
+                  `
+                },
+                confirmButtonColor: "#3085d6",
+                confirmButtonText: "Aceptar"
+            });
+        } else {
+            // Captar los elementos de los Inputs
+            $('#resultado').text(nombre);
+            $('#resultadoEmail').text(email);
+            $('#resultadoTextArea').text(mensaje);
+        }
     });
 });
