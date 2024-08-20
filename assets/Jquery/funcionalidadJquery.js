8ebd4bceeeb35484b33a42744a825fb16e941c45
Patgsj <patricio.gsj@gmail.com>
2024-08-19 20:39:48 -0400

    Funcionalidad Agregada el Alert

--- assets/Jquery/funcionalidadJquery.js
+++ assets/Jquery/funcionalidadJquery.js
@@ -16,13 +16,18 @@ $(document).ready(function() {
         event.preventDefault(); // Evita el envío del formulario
         
         // Obtener Valores
-        var nombre = $('#Nombre').val();
-        var email = $('#Email').val();
-        var mensaje = $('#floatingTextarea').val();
+        var nombre = $('#Nombre').val().trim(); // .trim() para eliminar espacios en blanco
+        var email = $('#Email').val().trim();
+        var mensaje = $('#floatingTextarea').val().trim();
         
-        // Captar los elemnetos del de los Inputs
-        $('#resultado').text(nombre);
-        $('#resultadoEmail').text(email);
-        $('#resultadoTextArea').text(mensaje);
+        // Verificar si los campos están completos
+        if (nombre === '' || email === '' || mensaje === '') {
+            alert('Por favor, complete todos los campos antes de enviar.');
+        } else {
+            // Captar los elementos de los Inputs
+            $('#resultado').text(nombre);
+            $('#resultadoEmail').text(email);
+            $('#resultadoTextArea').text(mensaje);
+        }
     });
 });
