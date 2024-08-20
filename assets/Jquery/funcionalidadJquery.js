229939e84fd332eaff89c4ea27a234649af621ba
Patgsj <patricio.gsj@gmail.com>
2024-08-19 20:08:09 -0400

    Mejoras Jquery

--- assets/Jquery/funcionalidadJquery.js
+++ assets/Jquery/funcionalidadJquery.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/* $(document).ready(function() {
     function actualizarResultado(selector, valor) {
         $(selector).html(valor);
     }
@@ -7,5 +7,22 @@ $(document).ready(function() {
         const valor = $(this).val();
         const target = $(this).data('target');
         actualizarResultado(target, valor);
+    });
+}); */
+
+
+$(document).ready(function() {
+    $('form').on('submit', function(event) {
+        event.preventDefault(); // Evita el envío del formulario
+        
+        // Obtener Valores
+        var nombre = $('#Nombre').val();
+        var email = $('#Email').val();
+        var mensaje = $('#floatingTextarea').val();
+        
+        // Captar los elemnetos del de los Inputs
+        $('#resultado').text(nombre);
+        $('#resultadoEmail').text(email);
+        $('#resultadoTextArea').text(mensaje);
     });
 });
